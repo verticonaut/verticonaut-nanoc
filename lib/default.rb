8dd2ec65cc4f7cc653eb6ca36feb12358c61a037
Martin Schweizer <contact@verticonaut.me>
2011-09-25 22:28:57 +0200

    Added subpages w base layouts.

--- lib/default.rb
+++ lib/default.rb
@@ -1,2 +1,19 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
+
+def base_color(detail_topic)
+  case detail_topic
+    when 'Climbing'
+      'background: #BFBF99;'
+    when 'Photography'
+      'background: #7A9982;'
+    when 'Projects'
+      'background: #665C47;'
+    when 'Thoughts'
+      'background: #7F0026;'
+    when 'About'
+      'background: #E5B867;'
+    else 'white'
+  end
+end
+
