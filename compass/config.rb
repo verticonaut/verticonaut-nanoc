e4e727a702f001628c436c147f297df4fc9e79dd
Martin Schweizer <contact@verticonaut.me>
2011-09-29 23:06:24 +0200

    CSS/Layout sassified using Compass. Exploring differen item types.

---
+++ compass/config.rb
@@ -0,0 +1,11 @@
+# I want to work with compass (see https://github.com/chriseppstein/compass/wiki/nanoc-Integration)
+http_path    = "/" 
+project_path = "." 
+css_dir      = "output/assets/style" 
+sass_dir     = "content/assets/style/sass" 
+images_dir   = "output/assets/images"
+
+# when using SCSS:
+sass_options = {
+  :syntax => :sass
+}
