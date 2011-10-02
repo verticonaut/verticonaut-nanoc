3a70fb6052704a997d5740f6706560e6513b94c5
Martin Schweizer <contact@verticonaut.me>
2011-10-02 18:25:17 +0200

    Refacotring. Close to finish porjects an photography galleries.

--- lib/custom.rb
+++ lib/custom.rb
@@ -1,3 +1,5 @@
 def fetch_galleries
-  @site[:galleries] ||= @site.items.select {|i| i.identifier =~ /photography\/galleries/}
+  @site[:galleries] ||= @site.items.
+    select {|i| i.identifier =~ /photography\/galleries\/.*\/_meta/}.
+    sort_by {|i| -1 * (i[:year] + i[:month])}
 end
