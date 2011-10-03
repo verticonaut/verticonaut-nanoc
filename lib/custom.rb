dc3d2a9ff92fbf95b1c184f8f2c5f1bc11348410
Martin Schweizer <contact@verticonaut.me>
2011-10-04 00:53:27 +0200

    Working on js galleries

--- lib/custom.rb
+++ lib/custom.rb
@@ -1,5 +1,9 @@
 def fetch_galleries
   @site[:galleries] ||= @site.items.
     select {|i| i.identifier =~ /photography\/galleries\/.*\/_meta/}.
-    sort_by {|i| -1 * (i[:year] + i[:month])}
+    sort_by {|i| -1 * (i[:year]*100 + i[:month])}
+end
+
+class Constants
+  MONTH = %w(- Jan Feb Mar Apr Mai Jun Jul Aug Sep Oct Nov Dec)
 end
